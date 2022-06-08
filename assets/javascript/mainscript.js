e76b375fb5b488ae71592b78037d5f7a4fd8bf5b
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-08 12:52:54 -0400

    added color changing to text areas based on hour of the day

--- assets/javascript/mainscript.js
+++ assets/javascript/mainscript.js
@@ -1,3 +1,5 @@
+var mainClock = moment().format("HH")
+console.log(mainClock);
 
 var scheduleItems = [];
 
@@ -10,3 +12,100 @@ setInterval(()=>{
     currentDay.textContent = now;
 }, 1000);
 
+var textColorChanger = function(){
+    if (mainClock < 9){
+       $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+
+    if (mainClock == 9) {
+        $("#hour-9").removeClass("future");
+        $("#hour-9").addClass("present");
+        $("#hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 10) {
+        $("#hour-9, #hour-10").removeClass("future");
+        $("#hour-10").addClass("present");
+        $("#hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 11) {
+        $("#hour-9, #hour-10, #hour-11").removeClass("future");
+        $("#hour-11").addClass("present");
+        $("#hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 12) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12").removeClass("future");
+        $("#hour-12").addClass("present");
+        $("#hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 13) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13").removeClass("future");
+        $("#hour-13").addClass("present");
+        $("#hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 14) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14").removeClass("future");
+        $("#hour-14").addClass("present");
+        $("#hour-15, #hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 15) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15").removeClass("future");
+        $("#hour-15").addClass("present");
+        $("#hour-16, #hour-17").addClass("future");
+    }
+    if (mainClock == 16) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16").removeClass("future");
+        $("#hour-16").addClass("present");
+        $("#hour-17").addClass("future");
+    }
+    if (mainClock == 17) {
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").removeClass("future");
+        $("#hour-17").addClass("present");
+    }
+
+    if (mainClock > 17) {
+        $("hour-17").removeClass("present");
+        $("#hour-17").addClass("past");
+    }
+    
+}
+textColorChanger();
+
+// switch(mainClock) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//         $("#hour")
+//         break;
+//     case 9:
+//         $("#hour-9").removeClass("future")
+//         $("#hour-9").addClass("present");
+//         break;
+//     case 10:
+//         break;
+//     case 11:
+//         break;
+//     case 12:
+//         break;
+//     case 13:
+//         break;
+//     case 14:
+//         break;
+//     case 15:
+//         break;
+//     case 16:
+//         break;
+//     case 17:
+//         break;
+//     case 18:
+//     case 19:
+//     case 20:
+//     case 21:
+//     case 22:
+//     case 23:
+//         break;
+// }
