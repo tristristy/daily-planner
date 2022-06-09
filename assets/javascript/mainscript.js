345e200cd3b09d21dbbb240c303b1259290bcc6a
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-09 08:28:05 -0400

    added buttons to each task

--- assets/javascript/mainscript.js
+++ assets/javascript/mainscript.js
@@ -5,11 +5,28 @@ var scheduleItems = [];
 
 const currentDay = document.getElementById("currentDay")
 
+var loadTextArray = function(){
+    JSON.parse(localStorage.getItem("workDayArray"));
+};
+
+var saveTextArray = function(){
+    localStorage.setItem("workDayArray", JSON.stringify(workDayArray));
+};
+
+$(".task-text").click(function(){
+    var value = $(".task").attr("contenteditable");
+    if (!value){
+        $(".task").attr("contenteditable", "true");
+    } else {
+        $(".task").attr("contenteditable", "false");
+    }
+});
 
 
 var textColorChanger = function(){
     if (mainClock < 9){
-       $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").removeClass("past");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
     }
 
     if (mainClock == 9) {
@@ -19,41 +36,49 @@ var textColorChanger = function(){
     }
     if (mainClock == 10) {
         $("#hour-9, #hour-10").removeClass("future");
+        $("#hour-9").addClass("past");
         $("#hour-10").addClass("present");
         $("#hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 11) {
         $("#hour-9, #hour-10, #hour-11").removeClass("future");
+        $("#hour-9, #hour-10").addClass("past");
         $("#hour-11").addClass("present");
         $("#hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 12) {
         $("#hour-9, #hour-10, #hour-11, #hour-12").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11").addClass("past");
         $("#hour-12").addClass("present");
         $("#hour-13, #hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 13) {
         $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12").addClass("past");
         $("#hour-13").addClass("present");
         $("#hour-14, #hour-15, #hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 14) {
         $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13").addClass("past");
         $("#hour-14").addClass("present");
         $("#hour-15, #hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 15) {
         $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14").addClass("past");
         $("#hour-15").addClass("present");
         $("#hour-16, #hour-17").addClass("future");
     }
     if (mainClock == 16) {
         $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15").addClass("past");
         $("#hour-16").addClass("present");
         $("#hour-17").addClass("future");
     }
     if (mainClock == 17) {
         $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17").removeClass("future");
+        $("#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16").addClass("past");
         $("#hour-17").addClass("present");
     }
 
